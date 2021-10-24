10b160683e8499fbbd99358311742fce7e4bbf60
angelptli <ali95@mail.ccsf.edu>
2021-10-23 19:18:26 -0700

    Create dim sum app to log dim sum items and details

---
+++ django_learning/dim_sum/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class DimSumItem(models.Model):
+    item_name    = models.CharField(max_length=100)
+    description  = models.TextField(null=True, blank=True)
+    normal_price = models.DecimalField(decimal_places=2, max_digits=1000)
+    history      = models.TextField(null=True, blank=True)
+    # Set default to True from makemigrations on the command line.
+    # This sets existing objects to have this default value.
+    tasty        = models.BooleanField()
+
+
+
+    def __str__(self):
+        return str.title(self.item_name)
