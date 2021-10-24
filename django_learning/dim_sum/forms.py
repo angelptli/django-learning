79fecb5236b4cbc67f60c0b4905bdcd2671a6d0b
angelptli <ali95@mail.ccsf.edu>
2021-10-23 23:20:17 -0700

    Define widget attributes for a form

--- django_learning/dim_sum/forms.py
+++ django_learning/dim_sum/forms.py
@@ -20,8 +20,17 @@ class AddItemForm(forms.ModelForm):
 
 
 class RawItemForm(forms.Form):
-    item_name    = forms.CharField()
-    description  = forms.CharField(required=False)
+    item_name    = forms.CharField(widget=forms.TextInput(attrs={
+                                          "placeholder": "Dim Sum Item"}))
+    description  = forms.CharField(
+                         required=False,
+                         widget=forms.Textarea(
+                                attrs={
+                                    "class": "new-class-name two",
+                                    "placeholder": "Dim sum's description",
+                                    "rows": 20,
+                                    "cols": 120
+                                }))
     normal_price = forms.DecimalField(required=False)
     history      = forms.CharField(required=False)
     tasty        = forms.CheckboxInput()
