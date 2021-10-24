c3b74adf735409cf525293301558c161f7e8c4d5
angelptli <ali95@mail.ccsf.edu>
2021-10-23 23:30:38 -0700

    Perform item name validation from views

--- django_learning/dim_sum/forms.py
+++ django_learning/dim_sum/forms.py
@@ -8,6 +8,21 @@ class AddItemForm(forms.ModelForm):
     can fill out in this form.
     """
 
+    item_name = forms.CharField(widget=forms.TextInput(attrs={
+                                          "placeholder": "Dim Sum Item"}))
+    description  = forms.CharField(
+                         required=False,
+                         widget=forms.Textarea(
+                                attrs={
+                                    "class": "new-class-name two",
+                                    "placeholder": "Dim sum's description",
+                                    "rows": 20,
+                                    "cols": 120
+                                }))
+    normal_price = forms.DecimalField(required=False)
+    history      = forms.CharField(required=False)
+    tasty        = forms.CheckboxInput()
+
     class Meta:
 
         model = DimSumItem
@@ -17,6 +32,15 @@ class AddItemForm(forms.ModelForm):
             'normal_price',
             'history'
         ]
+
+    def clean_item_name(self, *args, **kwargs):
+        """Raise validation error if item name contain invalid words."""
+        item_name = self.cleaned_data.get("item_name")
+        if "dim sum" in item_name:
+            raise forms.ValidationError("This is not a valid item name")
+        if "garbage" in item_name:
+            raise forms.ValidationError("This is not a valid item name")
+        return item_name
 
 
 class RawItemForm(forms.Form):
--- django_learning/dim_sum/views.py
+++ django_learning/dim_sum/views.py
@@ -9,28 +9,28 @@ def dimsum_detail_view(request):
     return render(request, "dim_sum/detail.html", {'items': items})
 
 
-# def add_item_view(request):
-#     form = AddItemForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         # Clear out form
-#         form = AddItemForm()
-
-#     context = {
-#         'form': form
-#     }
-
-#     return render(request, "dim_sum/add_item.html", context)
-
-
 def add_item_view(request):
-    form = RawItemForm()
+    form = AddItemForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        # Clear out form
+        form = AddItemForm()
 
-    if request.method == "POST":
-        form = RawItemForm(request.POST)
-        if form.is_valid():
-            DimSumItem.objects.create(**form.cleaned_data)
-
-    context = {'form': form}
+    context = {
+        'form': form
+    }
 
     return render(request, "dim_sum/add_item.html", context)
+
+
+# def add_item_view(request):
+#     form = RawItemForm()
+
+#     if request.method == "POST":
+#         form = RawItemForm(request.POST)
+#         if form.is_valid():
+#             DimSumItem.objects.create(**form.cleaned_data)
+
+#     context = {'form': form}
+
+#     return render(request, "dim_sum/add_item.html", context)
